afb3726135500406c769c7259d13a874cc5c74c8
notcl4y <notcl4y@gmail.com>
2025-12-28 14:25:10 +0300

    Add toggleanim button and refreshrate input

--- index.js
+++ index.js
@@ -4,14 +4,55 @@ const ctx = canvas.getContext("2d");
 const width = 512;
 const height = 512;
 
-const targetRefreshRate = 8;
-const interval = 1000 / targetRefreshRate;
+let animEnabled = true;
+let targetRefreshRate = 8;
+let interval = 1000 / targetRefreshRate;
 
 let lastTime = 0;
+
+function linkButton(id, click) {
+    const btn = document.getElementById(id);
+    btn.onclick = (e) => { click(e, btn); };
+}
+
+function linkInput(id, change) {
+  const input = document.getElementById(id);
+  input.onchange = (e) => { change(e, input); };
+}
+
+linkButton("btn-toggleanim", (e, t) => {
+    animEnabled = !animEnabled;
+
+    if (animEnabled) {
+        t.innerHTML = "Pause";
+        requestAnimationFrame(draw);
+    } else {
+        t.innerHTML = "Play";
+    }
+});
+
+linkInput("inp-refreshrate", (e, t) => {
+    let num = Number(t.value);
+
+    if (typeof(num) !== "number") {
+        return;
+    } else if (isNaN(num)) {
+        return;
+    }
+
+    targetRefreshRate = Math.trunc(num);
+    interval = 1000 / targetRefreshRate;
+    console.log(`targetRefreshRate = ${targetRefreshRate}`);
+    console.log(`interval = ${interval}`);
+});
 
 requestAnimationFrame(draw);
 
 function draw(timestamp) {
+    if (!animEnabled) {
+        return;
+    }
+
     if (timestamp - lastTime >= interval) {
         ctx.putImageData(generateNoise(width, height), 0, 0);
         lastTime = timestamp;
@@ -24,7 +65,7 @@ function generateNoise(width, height) {
 
     for (let i = 0; i < imageData.data.length; i += 4) {
         const color = Math.floor(Math.random() * 255);
-    
+
         imageData.data[i] = color;
         imageData.data[i + 1] = color;
         imageData.data[i + 2] = color;
